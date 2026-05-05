d6fdf774e07444d87d48139e7c461e75ad2f4f9c
chaduhduh <chadd.myers3@gmail.com>
2026-05-05 09:50:44 -0700

    installs SPARCL 1.3.0

--- dlstack_install.sh
+++ dlstack_install.sh
@@ -400,7 +400,7 @@ if [ $do_managers_only == 0 ]; then
     pip install pyopengl
     pip install pyzdcf
     pip install rebound==3.24.3
-    pip install sparclclient==1.2.8
+    pip install sparclclient==1.3.0
     pip install speclite
     pip install specutils
     pip install termcolor
